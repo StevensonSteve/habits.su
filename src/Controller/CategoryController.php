53955f9a2c7ee30650e0b331f81d4e415257dd1c
Stevenson <stevensonsteve393@gmail.com>
2026-08-09 11:11:47 +0300

    Добанленна функция добавления категорий с сайта

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -2,20 +2,24 @@
 
 namespace App\Controller;
 
+use DateTimeImmutable;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route('/category')]
 final class CategoryController extends AbstractController
 {
+    private const TEST_USER_ID = 1;
+
     #[Route('', name: 'category_index')]
     public function index(EntityManagerInterface $entityManager): Response 
     {
         $sql = 'SELECT * FROM categories WHERE user_id = :userId';
         $categories = $entityManager->getConnection()->executeQuery($sql, [
-            'userId' => 1,
+            'userId' => self::TEST_USER_ID,
         ])->fetchAllAssociative();
         
         // dd($categories);
@@ -35,5 +39,29 @@ final class CategoryController extends AbstractController
         ])->fetchAllAssociative();
 
         return $this->redirectToRoute('category_index');
+    }
+
+    #[Route('/new', name: 'category_new')]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response 
+    {
+        if ($request->getMethod() == 'POST') {
+            $name = $request->request->get('name');
+
+            $sql = "INSERT INTO categories (name, user_id, created_at, updated_at) 
+                VALUES (:name, :userId, :createdAt, :updatedAt)";
+
+            $entityManager->getConnection()->executeQuery($sql , [
+                'name' => $name,
+                'userId' => self::TEST_USER_ID,
+                'createdAt' => new DateTimeImmutable()->format("Y-m-d H:m:s"),
+                'updatedAt' => new DateTimeImmutable()->format("Y-m-d H:m:s"),
+            ])->fetchAllAssociative();
+
+            return $this->redirectToRoute('category_index');
+        }
+
+        return $this->render('category/new.html.twig', [
+            'categories' => [],
+        ]);
     }
 }
